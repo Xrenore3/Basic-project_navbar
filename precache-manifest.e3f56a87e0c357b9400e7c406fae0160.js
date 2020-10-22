a08ff25690cc6ec7552b9b5bc6421f1adfd1b02e
Xrenore3 <romanchaus519@gmail.com>
2020-10-23 00:20:00 +0300

    Updates

---
+++ precache-manifest.e3f56a87e0c357b9400e7c406fae0160.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b8c08529c2a1fd3f73b82868179e321",
+    "url": "/Basic-project_navbar/index.html"
+  },
+  {
+    "revision": "41c3dfab7bf87537794b",
+    "url": "/Basic-project_navbar/static/css/main.9144ac09.chunk.css"
+  },
+  {
+    "revision": "5036664f6a0cc62b8b04",
+    "url": "/Basic-project_navbar/static/js/2.41d20619.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Basic-project_navbar/static/js/2.41d20619.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "41c3dfab7bf87537794b",
+    "url": "/Basic-project_navbar/static/js/main.1b1c583b.chunk.js"
+  },
+  {
+    "revision": "42379e2dfd6a38dc9ef0",
+    "url": "/Basic-project_navbar/static/js/runtime-main.e5702d9f.js"
+  },
+  {
+    "revision": "2b686f2e5682570fb85fc952a4581419",
+    "url": "/Basic-project_navbar/static/media/logo.2b686f2e.svg"
+  }
+]);
